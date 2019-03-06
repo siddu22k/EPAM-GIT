a05e3578a17a9d0aaed15ba6e95b5e9c9ae7617d
siddu22k <siddhardhakommineni@gmail.com>
2019-03-06 21:44:28 +0530

    New-branch commits

--- Sample.java
+++ Sample.java
@@ -3,5 +3,7 @@ public class Sample
     public static void main(String args[])
     {
         System.out.println("Master Branch:Git is the version control system");
+        System.out.println("New-branch:Branch Creation is a fun ride");
+
     }
 }
